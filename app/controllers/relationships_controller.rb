e64ea220cc8bf3f5bae6da682c7c3ddf3240ddd8
justmartin <justin.lamar.martin@gmail.com>
2016-10-25 12:12:24 -0700

    Added ability to follow a user on User page.

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,10 +1,13 @@
-class RelationshipController < ApplicationController
+class RelationshipsController < ApplicationController
 
   def index
   end
 
   def create
-    
+    Relationship.create(follower_id: current_user.id,
+                        followee_id: params[:followee_id])
+
+    redirect_to :back
   end
 
 end
